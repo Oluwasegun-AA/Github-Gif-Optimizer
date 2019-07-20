a0048e18060123f16c526c9a241290bea6939613
shegsteham <oluwasegunadepoju@gmail.com>
2019-07-20 22:27:54 +0200

    fix() fix UI scroll

---
+++ ui/js/home.js
@@ -0,0 +1,17 @@
+// import electron, {ipcRenderer} from 'electron'
+
+// const select = document.querySelector.bind(document);
+// const file = select(file).file[0];
+
+
+const a = document.getElementById('file');
+a.addEventListener('click', b);
+
+function b (){
+  onmouseleavconsole.log('sdfghjk');
+}
+// const ko = ()=> {
+//   console.log('sdfghjk');
+// }
+
+b();
