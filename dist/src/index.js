dd05f8997a4483b459ffb40867c2870636c80be2
shegsteham <oluwasegunadepoju@gmail.com>
2019-08-13 16:41:50 +0200

    ft(menu): customise-main/dock-menu
    - customise main menu
    - customise dock menu
    - introduce toast notification
    
    [finishes]

--- dist/src/index.js
+++ dist/src/index.js
@@ -8,22 +8,88 @@ var _path = _interopRequireDefault(require("path"));
 
 var _fluentFfmpeg = _interopRequireDefault(require("fluent-ffmpeg"));
 
-let win;
+let mainWindow;
+let newWindow;
 const server = {
   get: (event, callback) => _electron.ipcMain.on(event, callback),
-  send: (event, callback) => win.webContents.send(event, callback)
+  send: (event, callback) => mainWindow.webContents.send(event, callback)
 };
+const menuTemplate = [{
+  label: 'VidInfo',
+  submenu: [{
+    label: 'Settings',
+
+    click() {
+      createNewWindow('ui/index.html', 'Settings');
+      new _electron.Notification({
+        title: 'notify',
+        body: 'I am warning you o!'
+      }).show();
+    }
+
+  }, {
+    label: 'Quit',
+    accelerator: 'CmdOrCtrl+Q',
+
+    click() {
+      _electron.app.quit();
+    }
+
+  }]
+}];
+process.platform === 'darwin' ? menuTemplate.unshift({
+  label: 'wink'
+}) : null;
+const dockTemplate = [{
+  label: 'New Window',
+
+  click() {
+    console.log('New Window');
+  }
+
+}, {
+  label: 'New Window with Settings',
+  submenu: [{
+    label: 'Basic'
+  }, {
+    label: 'Pro'
+  }]
+}, {
+  label: 'New Command...'
+}];
+
+const mainMenu = _electron.Menu.buildFromTemplate(menuTemplate);
+
+const dockMenu = _electron.Menu.buildFromTemplate(dockTemplate);
 
 _electron.app.on('ready', () => {
-  win = new _electron.BrowserWindow({
+  mainWindow = new _electron.BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      backgroundColor: 'red'
     }
   });
-  win.loadURL(`file:///${_path.default.resolve(`{__dirname}/../`, 'ui/index.html')}`);
+  mainWindow.loadURL(`file://${_path.default.resolve(`__dirname/../`, 'ui/index.html')}`);
+
+  _electron.Menu.setApplicationMenu(mainMenu);
+
+  _electron.app.dock.setMenu(dockMenu);
 });
+
+const createNewWindow = (filePath, title) => {
+  newWindow = new _electron.BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+      backgroundColor: 'red'
+    },
+    title
+  });
+  newWindow.loadURL(`file://${_path.default.resolve(`__dirname/../`, filePath)}`);
+};
 
 const ale = (event, path) => {
   _fluentFfmpeg.default.ffprobe(path, (err, metadata) => {
