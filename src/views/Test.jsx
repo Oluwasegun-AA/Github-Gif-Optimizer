450acd8c9ed9dea4bd6c3f91c3950256dd5a7bfa
shegsteham <oluwasegunadepoju@gmail.com>
2019-08-25 09:44:04 +0200

    ft(react): dispatch old workflow

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.png';
+import img from '../assets/logo.png';
 
 class Test extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={img} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
