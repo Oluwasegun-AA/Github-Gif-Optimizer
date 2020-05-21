2aee672b8130f69e3d2b684712d165252e13079e
shegsteham <oluwasegunadepoju@gmail.com>
2020-05-22 00:29:35 +0100

    update jsdoc

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,10 @@ import { App } from './client/components/index';
 import store from './client/store';
 import './client/styles/index.scss';
 
+/**
+ * setup React elements to be rendered within the HTML element with id = root
+ * setup Redux store as a parent prop to the application
+ */
 render(
   <Provider store={store}>
     <App />
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// configure enzyme-adapter-react for test purposes
 configure({
   adapter: new Adapter(),
 });
