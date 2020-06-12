77dd8f52273b1085fbf8ad7c44ad41d324b6acfb
shegsteham <oluwasegunadepoju@gmail.com>
2020-06-12 21:35:58 +0100

    implement file load, notification Modal and delete

--- src/client/components/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Routes from '../../routes';
-
-// setup base app
-const App = () => <div className="routes">{Routes}</div>;
-
-export default App;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import { Home } from './client/views/index';
+import { Home, FileLoaded } from './client/views/index';
 
 // setup browser router for url routing
 const Routes = (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/fileLoaded" component={FileLoaded} />
     </Switch>
   </BrowserRouter>
 );
