16e732a5be744ea4a8d2e20231cd6ec1aed1089c
shegsteham <oluwasegunadepoju@gmail.com>
2020-06-25 20:52:37 +0100

    adopt memoryRouter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Home from './client/views/index';
+import { Route, MemoryRouter, Switch } from 'react-router-dom';
+import { Home } from './client/views/index';
 
 // setup browser router for url routing
 const Routes = (
-  <BrowserRouter>
+  <MemoryRouter>
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
-  </BrowserRouter>
+  </MemoryRouter>
 );
 
 export default Routes;
